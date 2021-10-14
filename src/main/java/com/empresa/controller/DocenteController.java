0ffd344b83714ddd9689ab221e04fb7ea7184036
Ricardo88888 <dionisiocarpioricardojess@gmail.com>
2021-10-14 18:36:45 -0500

    La tarea profe

--- src/main/java/com/empresa/controller/DocenteController.java
+++ src/main/java/com/empresa/controller/DocenteController.java
@@ -6,17 +6,23 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entity.Docente;
+import com.empresa.entity.FiltroDocente;
 import com.empresa.service.DocenteService;
 import com.empresa.util.Constantes;
+
+
 
 @RestController
 @RequestMapping("/rest/docente")
@@ -51,4 +57,102 @@ public class DocenteController {
 		return ResponseEntity.ok(salida);
 	}
 
+	@GetMapping("/porDni/{paramDni}")
+	@ResponseBody
+	public ResponseEntity<List<Docente>> listaDocentePorDni(@PathVariable("paramDni")String dni) {
+		List<Docente> lista = docenteService.listaDocentePorDni(dni);
+		return ResponseEntity.ok(lista);
+	}
+	
+	@GetMapping("/porNombre/{paramNombre}")
+	@ResponseBody
+	public ResponseEntity<List<Docente>> listaDocentePorNombre(@PathVariable("paramNombre")String nombre) {
+		List<Docente> lista = docenteService.listaDocentePorNombre(nombre);
+		return ResponseEntity.ok(lista);
+	}
+	
+	@GetMapping("/porDniNombre/{paramDni}/{paramNombre}")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaDocentePorDniNombre(
+			@PathVariable("paramDni")String dni,
+			@PathVariable("paramNombre")String nombre) {
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			List<Docente> lista = docenteService . listaDocentePorDniNombre(dni, "%"+nombre+"%");
+			if (CollectionUtils.isEmpty(lista)) {
+				salida.put("mensaje", "No existe elementos para la consulta.");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos.");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	@GetMapping("/porDniNombreConParametros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaDocentePorDniNombreConParametros(
+			@RequestParam(value = "nombre", required = false, defaultValue = "") String nombre,
+			@RequestParam(value = "dni", required = false, defaultValue = "") String dni) {
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			List<Docente> lista = docenteService .listaDocentePorDniNombre(dni, "%"+nombre+"%");
+			if(CollectionUtils.isEmpty(lista)){
+				salida.put("mensaje", "No existe elementos para la consulta.");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos.");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	
+	@GetMapping("/porDniNombreUbigeoConParametros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaDocentePorDniNombreUbigeoConParametros(
+			@RequestParam(value = "nombre",required = false, defaultValue = "")String nombre,
+			@RequestParam(value = "dni",required = false, defaultValue = "")String dni,
+			@RequestParam(value = "idUbigeo",required = false, defaultValue = "0")int idUbigeo)
+	{
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			List<Docente> lista = docenteService . listaDocentePorDniNombreUbigeo(dni, "%"+nombre+"%", idUbigeo);
+			if (CollectionUtils.isEmpty(lista)) {
+				salida.put("mensaje", "No existe elementos para la consulta.");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos.");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	@GetMapping("/porDniNombreUbigeoConJson")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaDocentePorDniNombreUbigeoConParametros(
+			@RequestBody FiltroDocente filtro)
+	{
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			filtro.setNombre("%"+filtro.getNombre()+"%");
+			List<Docente> lista = docenteService . listaDocentePorFiltro(filtro);
+			if (CollectionUtils.isEmpty(lista)) {
+				salida.put("mensaje", "No existe elementos para la consulta.");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos.");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
 }
--- src/main/java/com/empresa/repository/ModalidadRepository.java
+++ src/main/java/com/empresa/repository/ModalidadRepository.java
@@ -1,10 +1,19 @@
 package com.empresa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.empresa.entity.FiltroModalidad;
 import com.empresa.entity.Modalidad;
 
 public interface ModalidadRepository extends JpaRepository<Modalidad, Integer>  {
 
-	
+	@Query("SELECT d FROM Modalidad d WHERE "
+			+ " (:#{#fil.nombre} is '' or d.nombre like :#{#fil.nombre} ) and "
+			+ " (:#{#fil.sede} is '' or d.sede like :#{#fil.sede} ) and "
+			+ " (:#{#fil.idDeporte} is 0  or d.deporte.idDeporte = :#{#fil.idDeporte} ) " )
+	public abstract List<Modalidad> listaModalidadPorFiltro(@Param("fil") FiltroModalidad filtro);
 }
